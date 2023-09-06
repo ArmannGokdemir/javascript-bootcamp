b0f42d30122a0c1c9d68d3d0424564dac942fc75
ArmannGokdemir <armangokde@gmail.com>
2023-09-07 01:08:03 +0300

    last commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+console.log(number);
+const numberInput = document.querySelector('.guess');
+const message = document.querySelector('.message');
+const secretNumber = document.querySelector('.number');
+const scoreLabel = document.querySelector('.score');
+
+const displayMessage = function (message) {
+  message.textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', () => {
+  if (numberInput.value < number) {
+    displayMessage('Too Low!');
+    score--;
+    scoreLabel.textContent = score;
+  } else if (numberInput.value > number) {
+    displayMessage('Too High!');
+    score--;
+    scoreLabel.textContent = score;
+  } else if (numberInput.value == number) {
+    message.textContent = 'You Won! 🏆';
+    secretNumber.textContent = number;
+    document.querySelector('.highscore').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+  }
+});
