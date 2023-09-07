0a979693ed74377d6213702938e70331e33baaaf
ArmannGokdemir <armangokde@gmail.com>
2023-09-08 02:18:36 +0300

    DOM Section

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+btnsOpenModal.forEach(element => {
+  element.addEventListener('click', () => {
+    modal.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+  });
+});
+
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
