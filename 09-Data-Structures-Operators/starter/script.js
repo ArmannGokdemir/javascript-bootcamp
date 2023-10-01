eddf1d4729a5e5f3d39fc8f75a2f9073e5635040
ArmannGokdemir <armangokde@gmail.com>
2023-10-01 22:08:38 +0300

    looping maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -114,6 +114,35 @@ const rest2 = {
   owner: "Giovanni Rossi",
 };
 
+//iterating maps
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct 🎉"],
+  [false, "Try again!"],
+]);
+console.log(question);
+
+//Convert object to map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+// let answer = "a";
+// while (answer !== question.get("correct") && answer !== 0) {
+//   answer = Number(prompt("Your answer"));
+//   console.log(answer);
+//   console.log(question.get(answer === question.get("correct")));
+// }
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+/*
 //maps
 const rest = new Map();
 rest.set("name", "Classico Italiano");
