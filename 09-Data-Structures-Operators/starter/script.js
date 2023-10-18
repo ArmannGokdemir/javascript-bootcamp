5d6cb63bb5db24b2a03c6ba6c4cbcc9346ca6947
ArmannGokdemir <armangokde@gmail.com>
2023-10-19 00:39:40 +0300

    strings coding challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -124,6 +124,7 @@ const passenger = "jessica ann smith davis";
 const capitalizeName = function (name) {
   const names = name.split(" ");
   const namesUpper = [];
+  console.log("names aaa ,", names);
   for (const n of names) {
     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
@@ -135,7 +136,7 @@ capitalizeName("arman gökdemir");
 
 //  padding
 const message = "go to gate 23";
-console.log(message.padStart(25, "+ ").padEnd(40, "+ "));
+console.log(message.padStart(25, "+").padEnd(40, "+ "));
 
 const maskCreditCard = function (number) {
   const str = number + ""; // or String() func to convert num to str
@@ -150,6 +151,57 @@ console.log(maskCreditCard("21331221484839018309"));
 
 const message2 = "Bad weather all departures delayed  ";
 console.log(message2.repeat(5));
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  let i = 0;
+  for (const txt of text.split("\n")) {
+    const newText = txt.split("_");
+    i++;
+    console.log(
+      newText[0].toLowerCase() +
+        newText[1][0].toUpperCase() +
+        newText[1].slice(1).toLowerCase().padEnd(20, "*") +
+        "✅".repeat(i)
+    );
+  }
+});
+
 /*
 //Working with strings
 const airline = "TAP Air Portugal";
