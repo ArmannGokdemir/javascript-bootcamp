0df45b48e7055a0c0b808931e945fafad2f8ed23
ArmannGokdemir <armangokde@gmail.com>
2023-10-20 00:53:24 +0300

    functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const bookings = [];
+const createBooking = function (
+  flightnum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  /*
+  ES5 way of setting default values
+  numPasssengers ||= 1;
+  price ||= 199;
+*/
+  const booking = {
+    flightnum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+console.log(bookings);
+
+createBooking('LH123');
+createBooking('LH123', 5);
+createBooking('LH123', 5, 320);
+createBooking('LH123', undefined, 1000);
